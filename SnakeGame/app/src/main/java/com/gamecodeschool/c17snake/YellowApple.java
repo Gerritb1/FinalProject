31a42b23d15ef678154e7856066e0641658c7e4f
masoudahang <144642951+masoudahang@users.noreply.github.com>
2024-04-12 21:07:33 -0700

    Created a YellowApple.java class.

---
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/YellowApple.java
@@ -0,0 +1,73 @@
+package com.gamecodeschool.c17snake;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import java.util.Random;
+
+class YellowApple extends GameObject implements Spawnable{
+
+    // An image to represent the apple
+    private Bitmap mBitmapApple;
+
+    public boolean spawned;
+
+    // Maintain a single global reference to the apple
+    private static YellowApple yellowApple;
+
+    /// Set up the apple in the constructor
+    private YellowApple(Context context, Point sr, int s) {
+
+        super(context, sr, s);
+
+        // Load the image to the bitmap
+        mBitmapApple = BitmapFactory.decodeResource(context.getResources(), R.drawable.yellowapple);
+
+        // Resize the bitmap
+        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, s, s, false);
+    }
+
+    // Provide access to the apple, creating it if necessary
+    public static YellowApple getYellowApple(Context context, Point sr, int s) {
+        if(yellowApple == null)
+            yellowApple = new YellowApple(context, sr, s);
+        return yellowApple;
+    }
+
+    // This is called every time an apple is eaten
+    @Override
+    public void spawn() {
+        // Choose two random values and place the apple
+        Random random = new Random();
+        location.x = random.nextInt(mSpawnRange.x) + 1;
+        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        spawned = true;
+    }
+
+    public boolean isSpawned() {
+        return spawned;
+    }
+
+    // Draw the apple
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+        //canvas.drawBitmap(mBitmapApple,
+        //        location.x * mSize, location.y * mSize, paint);
+        if (spawned) {
+            canvas.drawBitmap(mBitmapApple,
+                    location.x * size, location.y * size, paint);
+        }
+    }
+
+    // Method to hide the apple
+    @Override
+    public void hide() {
+        // Set the apple's location outside the visible screen
+        location.set(-1, -1); // Set the location outside the visible screen
+    }
+
+
+}
