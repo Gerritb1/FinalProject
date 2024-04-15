4d53755ff66cfbe6b646f2e9d0ba8f90ecc8ec1b
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-04-15 16:55:24 -0700

    Update and rename NameDrawer.java to TextDrawer.java
    Extracted drawTaptoPlay from SnakeGame into here

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/TextDrawer.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/TextDrawer.java
@@ -7,27 +7,27 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.view.SurfaceView;
 
-public class NameDrawer extends SurfaceView implements INames {
+public class TextDrawer extends SurfaceView implements IText {
     private DrawPauseButton getScreen;
     private final Canvas mCanvas;
     private final Paint mPaint;
     private final SnakeGame mSnakeGame;
 
     // Constructor of NameDrawer class with DI
-    public NameDrawer(Context context, Canvas canvas, Paint paint, SnakeGame snakeGame, DrawPauseButton getScreen) {
+    public TextDrawer(Context context, Canvas canvas, Paint paint, SnakeGame snakeGame) {
         super(context);
         this.mCanvas = canvas;
         this.mPaint = paint;
         this.mSnakeGame = snakeGame;
-        this.getScreen = getScreen;
     }
 
     // Setter method for injecting DrawPauseButton dependency
-    public void setDrawPauseButton(DrawPauseButton drawPauseButton) {
-        this.getScreen = drawPauseButton;
+    public void setDrawPauseButton(DrawPauseButton getScreen) {
+        this.getScreen = getScreen;
     }
 
     // Method for drawing names
+    @Override
     public void drawNames() {
         Point screenDimensions = getScreen.getScreenDimensions();
         int screenWidth = screenDimensions.x;
@@ -40,5 +40,28 @@ public class NameDrawer extends SurfaceView implements INames {
         mCanvas.drawText(getResources().getString(R.string.name3), xCoordinate, 120, mPaint);
         mCanvas.drawText(getResources().getString(R.string.name4), xCoordinate, 155, mPaint);
         mCanvas.drawText(getResources().getString(R.string.name5), xCoordinate, 190, mPaint);
+    }
+
+    // Method for drawing "Tap to play" message
+    @Override
+    public void drawTapToPlay() {
+        // Draw the "Tap to play" message if the game is initially paused
+        String message = getResources().getString(R.string.tap_to_play);
+
+        // Get the width and height of the message
+        float messageWidth = mPaint.measureText(message);
+        float messageHeight = mPaint.getFontMetrics().bottom - mPaint.getFontMetrics().top;
+
+        // Get the screen dimensions
+        Point screenDimensions = getScreen.getScreenDimensions();
+        int screenWidth = screenDimensions.x;
+        int screenHeight = screenDimensions.y;
+
+        // Calculate the position to center the text horizontally and vertically
+        float centerX = (screenWidth - messageWidth) / 2;
+        float centerY = (screenHeight + messageHeight) / 2;
+
+        // Draw the "Tap to play" message centered on the screen
+        mCanvas.drawText(message, centerX, centerY, mPaint);
     }
 }
