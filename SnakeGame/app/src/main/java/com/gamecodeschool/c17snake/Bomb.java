c37728e3eabcec0266963c6ef8b934a6b06703ae
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-05-05 11:20:30 -0700

    Update Bomb.java Debugging

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Bomb.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Bomb.java
@@ -6,38 +6,46 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.util.Log;
+import android.view.MotionEvent;
 
+
+import java.util.ArrayList;
 import java.util.Random;
 
-public class Bomb extends GameObject implements Spawnable{
+
+public class Bomb extends GameObject implements Spawnable {
 
     private Bitmap mBitmapBomb;
-
     private static Bomb mBomb;
     private boolean readyToExplode = false;
     protected boolean spawned;
+    private Point mShootDirection;
+    private Point mLocation;
+
+    private boolean isTriggerButtonPressed = false;
 
     public Bomb(Context context, Point location, int size) {
         super(context, location, size);
         mBitmapBomb = BitmapFactory.decodeResource(context.getResources(), R.drawable.bomb);
         mBitmapBomb = Bitmap.createScaledBitmap(mBitmapBomb, size, size, false);
+        mLocation = new Point(0, 0); // Initialize mLocation with default values
     }
 
-    // Provide access to the bomb, creating it if necessary
+    private void setTriggerButtonPressed(){ isTriggerButtonPressed = true;}
+
     public static Bomb getBomb(Context context, Point location, int size) {
         if (mBomb == null) {
             mBomb = new Bomb(context, location, size);
         }
-        return mBomb;
+            return mBomb;
     }
 
     @Override
     public void draw(Canvas canvas, Paint paint) {
         // Draw the bomb image at the location of the snake's mouth
-
         if (spawned) {
-            canvas.drawBitmap(mBitmapBomb,
-                    location.x * size, location.y * size, paint);
+            canvas.drawBitmap(mBitmapBomb, location.x * size, location.y * size, paint);
         }
         if (segmentLocations.size() > 0) {
             Point mouthLocation = segmentLocations.get(0);
@@ -45,21 +53,77 @@ public class Bomb extends GameObject implements Spawnable{
         }
     }
 
+    // Add log statement to calculateBombDirection method
+    public void shootBomb(MotionEvent motionEvent, boolean isTriggerButtonPressed) {
+        if (motionEvent != null) {
+            Log.d("Bomb", "motionEvent is not null");
+
+            if (isTriggerButtonPressed) {
+                Log.d("Bomb", "Trigger button is pressed");
+
+                if (!isReadyToExplode()) {
+                    Log.d("Bomb", "Bomb is not ready to explode");
+
+                    int touchX = (int) motionEvent.getX();
+                    int touchY = (int) motionEvent.getY();
+
+                    if (getLocation() != null) {
+                        Log.d("Bomb", "Location is not null");
+
+                        int directionX = touchX - getLocation().x;
+                        int directionY = touchY - getLocation().y;
+
+                        mShootDirection = new Point(directionX, directionY);
+
+                        getLocation().x += mShootDirection.x;
+                        getLocation().y += mShootDirection.y;
+
+                        if (segmentLocations != null) {
+                            Log.d("Bomb", "segmentLocations is not null");
+
+                            for (int i = segmentLocations.size() - 1; i > 0; i--) {
+                                segmentLocations.get(i).x = segmentLocations.get(i - 1).x;
+                                segmentLocations.get(i).y = segmentLocations.get(i - 1).y;
+                            }
+                        }
+                    } else {
+                        Log.d("Bomb", "Location is null");
+                    }
+                } else {
+                    Log.d("Bomb", "Bomb is ready to explode");
+                }
+            } else {
+                Log.d("Bomb", "Trigger button is not pressed");
+            }
+        } else {
+            Log.d("Bomb", "motionEvent is null");
+        }
+    }
+    public void setShootDirection(Point shootDirection) {
+        this.mShootDirection = shootDirection;
+    }
+
+    public Point getShootDirection() {
+        return mShootDirection;
+    }
+
+    public Point getLocation() {
+        return mLocation;
+    }
+
+    public ArrayList<Point> getSegmentLocations() {
+        return segmentLocations;
+    }
     @Override
     public void spawn() {
-        // Choose two random values and place the bomb
+            // Choose two random values and place the bomb
         Random random = new Random();
         location.x = random.nextInt(mSpawnRange.x) + 1;
         location.y = random.nextInt(mSpawnRange.y - 1) + 1;
         spawned = true;
     }
 
-    @Override
-    public void hide() {
-        location.set(-10, -10);
-    }
-
-
+    // Check for collision with the snake
     public void checkSnakeCollision(Snake snake) {
         if (snake.bigCheckDinner(location)) {
             readyToExplode = true;
@@ -67,6 +131,10 @@ public class Bomb extends GameObject implements Spawnable{
         }
     }
 
+    @Override
+    public void hide() {
+        location.set(-10, -10);
+    }
     public boolean isSpawned() {
         return spawned;
     }
