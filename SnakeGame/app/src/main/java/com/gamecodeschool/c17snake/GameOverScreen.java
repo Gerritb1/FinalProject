d0d52e56e32291a411f93f13366fef1226a2143a
Angel-Cal <125705151+Angel-Cal@users.noreply.github.com>
2024-05-08 15:57:33 -0700

    GameOver Screen is functional, still needs optimizing

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/GameOverScreen.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/GameOverScreen.java
@@ -1,44 +1,117 @@
 package com.gamecodeschool.c17snake;
 
+import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.core.content.res.ResourcesCompat;
 
-public class GameOverScreen extends SurfaceView {
-
+public class GameOverScreen extends SurfaceView implements Runnable {
+    Context gContext;
+    int score;
+    SurfaceHolder gSurfaceHolder;
+    Canvas gCanvas;
+    Paint gPaint;
+    TextDrawer textDrawer;
+    volatile boolean playing;
+    private Thread thread = null;
     public GameOverScreen(Context context, int score) {
         super(context);
-        drawGameOver(context, score);
+        this.score = score;
+        this.gContext = context;
 
+        innitDrawing();
+    }
+    public int getHighscore(int score){
+        if(score > GameOverActivity.Highscore)
+            GameOverActivity.Highscore = score;
+        return GameOverActivity.Highscore;
+
+    }
+
+    public void innitDrawing(){
+        gCanvas = new Canvas();
+        gPaint = new Paint();
+        gSurfaceHolder = getHolder();
+        gPaint = new Paint();
+        textDrawer = new TextDrawer(gContext, gCanvas, gPaint);
     }
 
 
     private void drawGameOver(Context context, int score){
-       SurfaceHolder gSurfaceHolder = getHolder();
-       Canvas gCanvas = gSurfaceHolder.lockCanvas();
-       Paint gPaint = new Paint();
-       TextDrawer textDrawer = new TextDrawer(context, gCanvas, gPaint);
+        if(gSurfaceHolder.getSurface().isValid()) {
+            gCanvas = gSurfaceHolder.lockCanvas();
+            gCanvas.drawColor(Color.BLACK);
+            Typeface customFont = ResourcesCompat.getFont(context, R.font.retro);
+            gPaint.setTypeface(customFont);
 
-       gCanvas.drawColor(Color.BLACK);
+            gPaint.setColor(Color.RED);
+            gPaint.setTextSize(150);
+            gCanvas.drawText("GAME OVER", 550, 150, gPaint);
 
-       Typeface customFont = ResourcesCompat.getFont(context, R.font.retro);
-       textDrawer.drawGameOver(customFont);
-       textDrawer.drawScore(score, customFont);
+            gPaint.setColor(Color.WHITE);
+            gPaint.setTextSize(75);
+            gCanvas.drawText("Your Score: " + score, 650, 250, gPaint);
 
-       gSurfaceHolder.unlockCanvasAndPost(gCanvas);
+            gCanvas.drawText("Highscore:  " + getHighscore(score), 650, 350, gPaint);
+
+            gCanvas.drawText("Tap To Play Again!", 550, 500, gPaint);
 
 
+//            gPaint.setColor(Color.WHITE);
+//            gPaint.setTypeface(customFont);
+//            gPaint.setTextSize(150);
+//            String scoreString = "" + score;
+//            String scoreMessage = "Your Score: ";
+//            gCanvas.drawText(" your score: ", 50, 50, gPaint);
 
+            //textDrawer.drawGameOver(customFont);
+            //textDrawer.drawScore(score, customFont);
+
+            gSurfaceHolder.unlockCanvasAndPost(gCanvas);
+        }
 
     }
+
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        if ((motionEvent.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+            if(gContext instanceof Activity){
+                SnakeGame.activityFlag = true;
+                ((Activity) gContext).finish();
+            }
+        }
+        return true;
+    }
+
+    public void pause() {
+        playing = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+
+    }
+
+    public void resume() {
+        playing = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+
+    @Override
+    public void run() {
+        while (playing) {
+                drawGameOver(gContext, score);
+        }
+    }
+
 
 }
