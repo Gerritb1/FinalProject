523b0e94747b974092117e136caa58de78b00cbe
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-05-10 04:25:48 -0700

    Update PoisonApple.java Added energy drink overlap stuff

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/PoisonApple.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/PoisonApple.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 class PoisonApple extends Apple implements Spawnable{
@@ -17,6 +20,12 @@ class PoisonApple extends Apple implements Spawnable{
 
     // Maintain a single global reference to the apple
     private static PoisonApple poisonApple;
+    private static Point locations = null;
+    private List<Point> rockLocations = new ArrayList<>();
+    private List<Point> trashLocations = new ArrayList<>();
+    private Point mAppleLocations = null;
+    private Point yAppleLocations = null;
+    private Point eDrinkLocations = null;
 
     /// Set up the apple in the constructor
     private PoisonApple(Context context, Point sr, int s) {
@@ -40,10 +49,43 @@ class PoisonApple extends Apple implements Spawnable{
     // This is called every time an apple is eaten
     @Override
     public void spawn() {
+        rockLocations = Rock.get_Locations();
+        trashLocations = Trash.get_Locations();
+        yAppleLocations = YellowApple.get_Location();
+        mAppleLocations = Apple.get_Location();
         // Choose two random values and place the apple
         Random random = new Random();
         location.x = random.nextInt(mSpawnRange.x) + 1;
         location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        for (int x=0; x<4; x++){
+            if((location.x==rockLocations.get(x).x || location.x==(rockLocations.get(x).x)+1 || location.x==(rockLocations.get(x).x)+2 || location.x==(rockLocations.get(x).x)-1 || location.x==(rockLocations.get(x).x)-2) && (location.y==rockLocations.get(x).y || location.y==(rockLocations.get(x).y+1) || location.y==(rockLocations.get(x).y+2) || location.y==(rockLocations.get(x).y-1) || location.y==(rockLocations.get(x).y-2))){
+                location.x = random.nextInt(mSpawnRange.x) + 1;
+                location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+            }
+            if((location.x==mAppleLocations.x || location.x==mAppleLocations.x+1 || location.x==mAppleLocations.x-1) && (location.y==mAppleLocations.y || location.y==mAppleLocations.y+1 || location.y==mAppleLocations.y-1)){
+                location.x = random.nextInt(mSpawnRange.x) + 1;
+                location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+            }
+            if(yAppleLocations != null){
+                if((location.x==yAppleLocations.x || location.x==yAppleLocations.x+1 || location.x==yAppleLocations.x-1) && (location.y==yAppleLocations.y || location.y==yAppleLocations.y+1 || location.y==yAppleLocations.y-1)){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+            if(!trashLocations.isEmpty()){
+                if((location.x==trashLocations.get(x).x || location.x==(trashLocations.get(x).x)+1 || location.x==(trashLocations.get(x).x)-1) && (location.y==trashLocations.get(x).y || location.y==(trashLocations.get(x).y+1) || location.y==(trashLocations.get(x).y-1))){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+            if(eDrinkLocations != null){
+                if((location.x==eDrinkLocations.x || location.x==eDrinkLocations.x+1 || location.x==eDrinkLocations.x-1) && (location.y==eDrinkLocations.y || location.y==eDrinkLocations.y+1 || location.y==eDrinkLocations.y-1)){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+        }
+        locations = new Point(location.x, location.y);
         spawned = true;
     }
 
@@ -68,5 +110,11 @@ class PoisonApple extends Apple implements Spawnable{
         // Set the apple's location outside the visible screen
         location.set(-1, -1); // Set the location outside the visible screen
         spawned = false;
+    }
+    public static Point get_Location(){
+        return locations;
+    }
+    public static void remove_Locations(){
+        locations = null;
     }
 }
