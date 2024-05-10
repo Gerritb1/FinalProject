4a80b36feaa9b72141d4118255f31083f4b49ac0
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-05-10 04:22:52 -0700

    Update Trash.java Added energy drink overlap stuff

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Trash.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Trash.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Trash extends Rock implements Spawnable{
@@ -14,6 +17,13 @@ public class Trash extends Rock implements Spawnable{
     private static Trash trash2;
     private static Trash trash3;
     private static Trash trash4;
+    private static List<Point> locations = new ArrayList<>();
+    private List<Point> rockLocations = new ArrayList<>();
+    private Point mAppleLocations = null;
+    private Point yAppleLocations = null;
+    private Point pAppleLocations = null;
+    private Point eDrinkLocations = null;
+
 
     /// Set up the rock in the constructor
     private Trash(Context context, Point sr, int s) {
@@ -30,12 +40,46 @@ public class Trash extends Rock implements Spawnable{
     // This is called when the chance spawn works
     @Override
     public void spawn() {
+        rockLocations = Rock.get_Locations();
+        yAppleLocations = YellowApple.get_Location();
+        mAppleLocations = Apple.get_Location();
+        pAppleLocations = PoisonApple.get_Location();
         // Choose two random values and place the trash
         Random random = new Random();
         location.x = random.nextInt(mSpawnRange.x) + 1;
         location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        for (int x=0; x<4; x++){
+            if((location.x==rockLocations.get(x).x || location.x==(rockLocations.get(x).x)+1 || location.x==(rockLocations.get(x).x)+2 || location.x==(rockLocations.get(x).x)-1 || location.x==(rockLocations.get(x).x)-2) && (location.y==rockLocations.get(x).y || location.y==(rockLocations.get(x).y+1) || location.y==(rockLocations.get(x).y+2) || location.y==(rockLocations.get(x).y-1) || location.y==(rockLocations.get(x).y-2))){
+                location.x = random.nextInt(mSpawnRange.x) + 1;
+                location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+            }
+            if(locations.size() > x) {
+                if ((location.x == locations.get(x).x || location.x == (locations.get(x).x) + 1 || location.x == (locations.get(x).x) - 1) && (location.y == locations.get(x).y || location.y == (locations.get(x).y + 1) || location.y == (locations.get(x).y - 1))) {
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+            if(yAppleLocations != null){
+                if((location.x==yAppleLocations.x || location.x==yAppleLocations.x+1 || location.x==yAppleLocations.x-1) && (location.y==yAppleLocations.y || location.y==yAppleLocations.y+1 || location.y==yAppleLocations.y-1)){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+            if(pAppleLocations != null){
+                if((location.x==pAppleLocations.x || location.x==pAppleLocations.x+1 || location.x==pAppleLocations.x-1) && (location.y==pAppleLocations.y || location.y==pAppleLocations.y+1 || location.y==pAppleLocations.y-1)){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+            if(eDrinkLocations != null){
+                if((location.x==eDrinkLocations.x || location.x==eDrinkLocations.x+1 || location.x==eDrinkLocations.x-1) && (location.y==eDrinkLocations.y || location.y==eDrinkLocations.y+1 || location.y==eDrinkLocations.y-1)){
+                    location.x = random.nextInt(mSpawnRange.x) + 1;
+                    location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+                }
+            }
+        }
+        locations.add(new Point(location.x, location.y));
     }
-
 
     // Draw the trash
     @Override
@@ -50,9 +94,11 @@ public class Trash extends Rock implements Spawnable{
         location.set(-10, -10); // Set the location outside the visible screen
     }
 
-    public boolean chanceToSpawn(int score){
+    public void chanceToSpawn(int score, int chance){
         Random rand = new Random();
-        return rand.nextInt(score) > 3;
+        if (rand.nextInt(score) > chance) {
+            this.spawn();
+        }
     }
 
     // Provide access to the trash, creating it if necessary
@@ -77,6 +123,12 @@ public class Trash extends Rock implements Spawnable{
         if(trash4 == null)
             trash4 = new Trash(context, sr, s);
         return trash4;
+    }
+    public static List<Point> get_Locations(){
+        return locations;
+    }
+    public static void remove_Locations(){
+        locations = new ArrayList<>();
     }
 
 }
