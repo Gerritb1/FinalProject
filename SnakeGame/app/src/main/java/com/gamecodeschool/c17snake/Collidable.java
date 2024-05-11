b513b255259fec3a5aad2f9d5792719ee4b3ea6b
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-05-11 13:04:57 -0700

    Update Collidable.java checkDrink and checkBigDinner to strictly adhere to...
    the strategy design pattern

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Collidable.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/Collidable.java
@@ -5,4 +5,6 @@ import android.graphics.Point;
 public interface Collidable {
     boolean detectDeath();
     boolean checkDinner(Point l);
+    boolean bigCheckDinner(Point l);
+    boolean checkDrink(Point l);
 }
