64b4ef5af4dedcd0bd26c26138e1e26e0b3d5e9f
Phillip Harris <125616858+HKtrill@users.noreply.github.com>
2024-05-10 17:30:31 -0700

    Update TextDrawer.java Angel Game Over TextDrawer stuff

--- SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/TextDrawer.java
+++ SnakeGame/app/src/main/java/com/gamecodeschool/c17snake/TextDrawer.java
@@ -5,41 +5,28 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.view.SurfaceView;
+import android.graphics.Typeface;
+
 
 import java.util.ArrayList;
 
-public class TextDrawer extends SurfaceView implements IText {
-
-    private static Apple mApple;
-    private static Snake mSnake;
-    private DrawPauseButton getScreen;
+public class TextDrawer extends Screens{
     private final Canvas mCanvas;
     private final Paint mPaint;
-    private final SnakeGame mSnakeGame;
-    private int mScore;
-    private int ss;
-    private YellowApple yApple;
-    private ArrayList<Rock> rocks;
-    private Point mr;
+
 
 
     // Constructor of NameDrawer class with DI
-    public TextDrawer(Context context, Canvas canvas, Paint paint, SnakeGame snakeGame) {
+    public TextDrawer(Context context, Canvas canvas, Paint paint) {
         super(context);
         this.mCanvas = canvas;
         this.mPaint = paint;
-        this.mSnakeGame = snakeGame;
     }
 
-    // Setter method for injecting DrawPauseButton dependency
-    public void setDrawPauseButton(DrawPauseButton getScreen) {this.getScreen = getScreen; }
-    public void setSnake(Snake snake){ mSnake = snake;}
-
     // Method for drawing names
-    @Override
-    public void drawNames() { //Extracted from SnakeGame
-        Point screenDimensions = getScreen.getScreenDimensions();
+    public void drawNames(Typeface customFont) { //Extracted from SnakeGame
+        mPaint.setTypeface(customFont);
+        Point screenDimensions = getScreenDimensions();
         int screenWidth = screenDimensions.x;
         int xCoordinate = screenWidth - 340; // Adjust this value as needed
 
@@ -53,26 +40,43 @@ public class TextDrawer extends SurfaceView implements IText {
     }
 
     // Method for drawing "Tap to play" message
-    @Override
-    public void drawTapToPlay() { //Extracted from SnakeGame
+
+    public void drawTapToPlay(Typeface customFont) { //Extracted from SnakeGame
         // Draw the "Tap to play" message if the game is initially paused
+        mPaint.setTypeface(customFont);
+        mPaint.setTextSize(175);
+        mPaint.setColor(Color.RED);
         String message = getResources().getString(R.string.tap_to_play);
-
-        // Get the width and height of the message
-        float messageWidth = mPaint.measureText(message);
-        float messageHeight = mPaint.getFontMetrics().bottom - mPaint.getFontMetrics().top;
-
-        // Get the screen dimensions
-        Point screenDimensions = getScreen.getScreenDimensions();
-        int screenWidth = screenDimensions.x;
-        int screenHeight = screenDimensions.y;
-
-        // Calculate the position to center the text horizontally and vertically
-        float centerX = (screenWidth - messageWidth) / 2;
-        float centerY = (screenHeight + messageHeight) / 2;
+        Point centerPoint = getCenterPoint(getScreenDimensions(), getMessageDimensions(message, mPaint));
 
         // Draw the "Tap to play" message centered on the screen
-        mCanvas.drawText(message, centerX, centerY, mPaint);
+        mCanvas.drawText(message, centerPoint.x, centerPoint.y, mPaint);
+    }
+
+    public void drawGameOver(Typeface customFont){
+        mPaint.setColor(Color.RED);
+        mPaint.setTypeface(customFont);
+        mPaint.setTextSize(100);
+        String message = getResources().getString(R.string.GameOver);
+
+        Point centerPoint = getCenterPoint(getScreenDimensions(), getMessageDimensions(message, mPaint));
+
+        mCanvas.drawText(message, centerPoint.x, centerPoint.y/4, mPaint );
+
+
+    }
+
+    public void drawScore(int score, Typeface customFont){
+        mPaint.setColor(Color.WHITE);
+        mPaint.setTypeface(customFont);
+        mPaint.setTextSize(150);
+        String scoreString = "" + score;
+        String scoreMessage = "Your Score: ";
+
+        Point centerPoint = getCenterPoint(getScreenDimensions(), getMessageDimensions(scoreString, mPaint));
+
+        mCanvas.drawText(scoreString, centerPoint.x, 50, mPaint);
+        mCanvas.drawText(scoreMessage, centerPoint.x - 20, 50, mPaint);
     }
 
 }
